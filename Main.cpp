a58729216b0312a7ed36c160a78bc105df5a7b69
Pablo Alejandro Perez Acosta <alejandro.perez.acosta@hotmail.com>
2021-02-24 15:32:38 -0500

    Implementación de Recursividad

--- Main.cpp
+++ Main.cpp
@@ -101,6 +101,44 @@ public:
     }
 };
 
+// Allocates a new node with given data
+node *newNode(int data)
+{
+    node *new_node = new node;
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+ 
+// Function to insert a new node at the
+// end of linked list using recursion.
+node* insertEnd(node* head, int data)
+{
+    // If linked list is empty, create a 
+    // new node (Assuming newNode() allocates
+    // a new node with given data)
+    if (head == NULL) 
+         return newNode(data);
+ 
+    // If we have not reached end, keep traversing
+    // recursively.
+    else
+        head->next = insertEnd(head->next, data);
+    return head;
+}
+ 
+void traverse(node* head)
+{
+    if (head == NULL)
+       return;
+     
+    // If head is not NULL, print current node
+    // and recur for remaining list   
+    cout << head->data << " "<< endl;
+ 
+    traverse(head->next);
+}
+
 void Iterative()
 {
     //
@@ -141,6 +179,14 @@ void Recursive()
     cout<<"RECURSIVE                                "<< endl;
     cout<<"-----------------------------------------"<< endl;
 
+    //
+    node* head = NULL;
+    for (int i = 0; i < 10; i++)
+        head = insertEnd(head, i);
+
+    //
+    traverse(head);
+    
     //
     system("pause");
 }
